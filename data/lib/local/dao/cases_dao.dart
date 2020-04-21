37b08946250c332973bda3819c60d14ec5c7e112
Fordyta Abubakar <dhytodev@gmail.com>
2020-04-21 11:09:28 +0800

    case dao : add query select and insert

---
+++ data/lib/local/dao/cases_dao.dart
@@ -0,0 +1,16 @@
+
+import 'package:data/local/database/database.dart';
+import 'package:data/local/entities/case_entity.dart';
+import 'package:moor/moor.dart';
+
+part 'cases_dao.g.dart';
+
+@UseDao(tables: [Cases])
+class CasesDao extends DatabaseAccessor<CovidDatabase> with _$CasesDaoMixin {
+  CasesDao(CovidDatabase db) : super(db);
+
+  Future<List<CaseEntity>> getAllCases() => select(cases).get();
+
+  Future<int> insertCase(Insertable<CaseEntity> caseEntity) => into(cases).insert(caseEntity);
+
+}
